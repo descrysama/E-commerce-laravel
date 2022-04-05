8916fe7ec1e531dc3ec29fef700c318aeed916b7
Sarah Tahar <92088786+AceRiverBat@users.noreply.github.com>
2022-04-05 15:12:09 +0200

    header test

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,28 @@
 @extends('layouts.template')
 @section("header")
-  @if (Route::has('register'))
-    <a class="nav-link active" href="{{ route('register') }}">S'inscrire</a>
-  @endif
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand " href="{{ url('/welcome') }}"><img src="{{ asset('img/logo-plantis.png')}}" alt="plantis" width="50%"></a>
+            <div class="d-flex">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        @if (Route::has('login'))
+                        @auth
+                        <a class="nav-link active" href="{{ url('/dashboard') }}">Dashboard</a>
+                        @else
+                        <a class="nav-link active" href="{{ route('login') }}">Se connecter</a>
+                        @endauth
+                        @endif
+                        <a class="nav-link active" href="#">PANIER</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div>
+        <hr>
+    </div>
 @endsection
