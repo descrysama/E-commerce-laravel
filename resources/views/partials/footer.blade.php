0e42aeb042eb6bd9e0f5458cc283a9009dee1d31
Sarah Tahar <92088786+AceRiverBat@users.noreply.github.com>
2022-04-05 16:24:54 +0200

    footer encours

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,16 @@
+<div>
+        <hr>
+    </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+        <a class="nav-link active" href="#">A PROPOS</a>
+<div class="d-flex justify-content-center">
+        <a class="navbar-brand " href="{{ url('/') }}"><img src="{{ asset('img/logo-plantis.png')}}" alt="plantis" width="50%"></a>
+        </div>
+        <div class="d-flex">
+                        <a class="nav-link active" href="#">icone insta</a>
+                        <a class="nav-link active" href="#">icone facebook</a>
+                        <a class="nav-link active" href="#">icone pinterest</a>
+            </div>
+        </div>
+    </nav>
