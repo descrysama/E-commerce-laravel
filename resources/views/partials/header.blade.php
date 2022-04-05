8d69791c79f1149601b7fee144dabb153f68b5cd
Sarah Tahar <92088786+AceRiverBat@users.noreply.github.com>
2022-04-05 15:25:43 +0200

    header fini

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,7 @@
-@extends('layouts.template')
-@section("header")
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand " href="{{ url('/welcome') }}"><img src="{{ asset('img/logo-plantis.png')}}" alt="plantis" width="50%"></a>
+            <a class="navbar-brand " href="{{ url('/') }}"><img src="{{ asset('img/logo-plantis.png')}}" alt="plantis" width="50%"></a>
             <div class="d-flex">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -25,4 +24,3 @@
     <div>
         <hr>
     </div>
-@endsection
