a6e2994158ef9c2875f3a55242351220cc0e1142
DescrySama <pierrehugot35@gmail.com>
2022-04-06 15:29:07 +0200

    panier h3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,13 +7,13 @@
         <div class="row d-flex justify-content-center">
             @foreach ($articles as $article)
             <div class="col-md-3 col-12 col-lg-3 m-2 d-flex justify-content-center">
-                <div class="card" style="width: 18rem;">
+                <div class="card">
                     <a href="{{ url('/article' , $article->id) }}"><img src="{{$article -> image}}" class="card-img-top" alt="..."></a>
                     <div class="card-body">
                         <div class="d-flex">
                             <h6 class="card-title">{{$article -> nom}}</h6>
                             <div class="d-flex justify-content-end ms-auto">
-                                <h6 class="card-title">{{$article -> prix}}</h6>
+                                <h6 class="card-title">{{$article -> prix}}€</h6>
                             </div>
                         </div>
                     </div>
