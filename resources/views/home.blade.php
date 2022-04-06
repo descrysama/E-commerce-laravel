5a59349608b4d943f22976b388584071768e16b5
Sarah Tahar <92088786+AceRiverBat@users.noreply.github.com>
2022-04-06 10:37:34 +0200

    home fini

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,39 +3,48 @@
 @section('content')
 
 <div class="container">
-<div class="d-flex justify-content-evenly">
-    <div class="row">
-        <div class="col-md-4 col-12 col-lg-4">
-            <div class="card" style="width: 18rem;">
-                <img src="{{ asset('img/plante1.jpg') }}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="d-flex justify-content-evenly">
+        <div class="row">
+            <div class="col-md-4 col-12 col-lg-4">
+                <div class="card" style="width: 18rem;">
+                    <a href="#"><img src="{{ asset('img/plante1.jpg') }}" class="card-img-top" alt="..."></a>
+                    <div class="card-body">
+                        <div class="d-flex">
+                            <h6 class="card-title">Nom plante</h6>
+                            <div class="d-flex justify-content-end ms-auto">
+                                <h6 class="card-title">Prix</h6>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-4 col-12 col-lg-4">
-            <div class="card" style="width: 18rem;">
-                <img src="{ asset('img/plante2.jpg') }}" class="card-img-top" alt="2">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+            <div class="col-md-4 col-12 col-lg-4">
+                <div class="card" style="width: 18rem;">
+                    <a href="#"><img src="{{ asset('img/plante2.jpg') }}" class="card-img-top" alt="2"></a>
+                    <div class="card-body">
+                        <div class="d-flex">
+                            <h6 class="card-title">Nom plante</h6>
+                            <div class="d-flex justify-content-end ms-auto">
+                                <h6 class="card-title">Prix</h6>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-4 col-12 col-lg-4">
-            <div class="card" style="width: 18rem;">
-                <img src="{ asset('img/plante3.jpg') }}" class="card-img-top" alt="3">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+            <div class="col-md-4 col-12 col-lg-4">
+                <div class="card" style="width: 18rem;">
+                    <a href="#"><img src="{{ asset('img/plante3.jpg') }}" class="card-img-top" alt="3"></a>
+                    <div class="card-body">
+                        <div class="d-flex">
+                            <h6 class="card-title">Nom plante</h6>
+                            <div class="d-flex justify-content-end ms-auto">
+                                <h6 class="card-title">Prix</h6>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 </div>
 @endsection
