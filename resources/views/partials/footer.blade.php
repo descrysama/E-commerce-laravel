f1e20b4b11ed8441db83f51ac4c0fccc321e11d8
Sarah Tahar <92088786+AceRiverBat@users.noreply.github.com>
2022-04-08 09:52:29 +0200

    liens reseau

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -8,9 +8,9 @@
             <a class="navbar-brand " href="{{ url('/') }}"><img src="{{ asset('img/logo-plantis.png')}}" alt="plantis" width="125rem"></a>
         </div>
         <div class="d-flex">
-            <a class="nav-link active" href="#"><i class="fa-brands fa-instagram fa-xl"></i></a>
-            <a class="nav-link active" href="#"><i class="fa-brands fa-facebook-f fa-xl"></i></a>
-            <a class="nav-link active" href="#"><i class="fa-brands fa-pinterest-p fa-xl"></i></a>
+            <a class="nav-link active" href="https://www.instagram.com/?hl=fr"><i class="fa-brands fa-instagram fa-xl"></i></a>
+            <a class="nav-link active" href="https://www.facebook.com"><i class="fa-brands fa-facebook-f fa-xl"></i></a>
+            <a class="nav-link active" href="https://www.pinterest.fr"><i class="fa-brands fa-pinterest-p fa-xl"></i></a>
         </div>
     </div>
 </nav>
